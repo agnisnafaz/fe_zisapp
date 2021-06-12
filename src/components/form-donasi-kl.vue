e150633e32bc7127873bc9970c1defda9d0fffa2
trisnamaulida <60949474+TrisnaMaulida@users.noreply.github.com>
2021-06-12 22:18:04 +0700

    update build

---
+++ src/components/form-donasi-kl.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-sm-12">
+        <px-card :actions="false">
+          <div slot="with-padding">
+            <b-form class="needs-validation" @submit="onsubmit">
+              <div class="form-row">
+                <div class="col-md-4 mb-1">
+                  <label for="c_form_muzaki">Muzaki</label>
+                  <v-autocomplete
+                    :items="muzaki"
+                    item-text="label"
+                    auto-select-first
+                    return-object
+                    outlined
+                    required
+                    small
+                  >
+                  </v-autocomplete>
+                </div>
+                <div class="col-md-4 mb-1">
+                  <label for="c_form_program">Program</label>
+                  <v-autocomplete
+                    :items="program"
+                    item-text="label"
+                    auto-select-first
+                    return-object
+                    outlined
+                    required
+                    small
+                  >
+                  </v-autocomplete>
+                </div>
+                <div class="col-md-4 mb-1">
+                  <label for="c_form_bank">Bank</label>
+                  <v-autocomplete
+                    :items="bank"
+                    item-text="label"
+                    auto-select-first
+                    return-object
+                    outlined
+                    required
+                    small
+                  >
+                  </v-autocomplete>
+              
+              </div>
+              <div class="form-row">
+                <div class="col-md-4 mb-1">
+                  <label for="c_form_buktifisik">No Bukti Fisik</label>
+                  <b-form-input
+                    type="text"
+                    placeholder="No.Bukti Fisik"
+                  ></b-form-input>
+                </div>
+              </div>
+             
+              <b-button type="submit" variant="primary">Simpan</b-button>
+            </b-form>
+          </div>
+        </px-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Pxcard from "./Pxcard.vue";
+export default {
+  components: { Pxcard },
+  data() {
+    return {
+      periode: null,
+      period: [
+        { value: null, text: "2017" },
+        { value: "", text: "2018" },
+        { value: "", text: "2019" },
+      ],
+      metode: null,
+      pembayaran: [
+        { value: null, text: "Pilih" },
+        { value: "", text: "CASH" },
+        { value: "", text: "MUTASI BANK" },
+      ],
+    };
+  },
+  methods: {
+    onsubmit() {},
+  },
+};
+</script>
