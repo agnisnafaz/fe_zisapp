cf4f1b3c0e9f97f52dbadf5026c7617acceb5859
trisnamaulida <60949474+TrisnaMaulida@users.noreply.github.com>
2021-06-21 22:34:04 +0700

    getdata form donasi

--- src/components/form-donasi.vue
+++ src/components/form-donasi.vue
@@ -48,8 +48,8 @@
                   <label for="c_form_periode">Periode</label>
 
                   <b-form-select
-                    v-model="periode"
-                    :options="period"
+                    v-model="form.periode"
+                    :options="optionperiode"
                   ></b-form-select>
                 </div>
                 <div class="col-md-3 ">
@@ -61,8 +61,8 @@
                   <label for="c_form_periode">Metode Pembayaran</label>
 
                   <b-form-select
-                    v-model="metode"
-                    :options="pembayaran"
+                    v-model="form.metode_pembayaran"
+                    :options="optionmetode"
                   ></b-form-select>
                 </div>
               </div>
@@ -119,31 +119,71 @@
 
 <script>
 import Pxcard from "./Pxcard.vue";
+import API from "@/services/api.service";
 export default {
   components: { Pxcard },
   data() {
     return {
-      periode: null,
-      period: [
-        { value: null, text: "2017" },
-        { value: "", text: "2018" },
-        { value: "", text: "2019" },
-      ],
-      metode: null,
-      pembayaran: [
-        { value: null, text: "Pilih" },
-        { value: "", text: "CASH" },
-        { value: "", text: "MUTASI BANK" },
-      ],
+      form: {},
+      optionmetode: [],
+      optionperiode: [],
       muzaki: [],
       bank: [],
       program: [],
     };
   },
   methods: {
-    getMuzaki() {},
-    getBank() {},
-    getProgram() {},
+    getMuzaki() {
+      API.get("/api/muzaki").then(({ status, data }) => {
+        console.log(data);
+        if (status == 200 || status == 201) {
+          // reponse dari be jika berhasil
+
+          if (data.status) {
+            //berhasil
+            this.kantorlayanan = data.data;
+          } else {
+            //notifikasi gagal
+          }
+        } else {
+          //notifikasi gagal
+        }
+      });
+    },
+    getProgram() {
+      API.get("/api/program").then(({ status, data }) => {
+        console.log(data);
+        if (status == 200 || status == 201) {
+          // reponse dari be jika berhasil
+
+          if (data.status) {
+            //berhasil
+            this.kantorlayanan = data.data;
+          } else {
+            //notifikasi gagal
+          }
+        } else {
+          //notifikasi gagal
+        }
+      });
+    },
+    getBank() {
+      API.get("/api/bank").then(({ status, data }) => {
+        console.log(data);
+        if (status == 200 || status == 201) {
+          // reponse dari be jika berhasil
+
+          if (data.status) {
+            //berhasil
+            this.kantorlayanan = data.data;
+          } else {
+            //notifikasi gagal
+          }
+        } else {
+          //notifikasi gagal
+        }
+      });
+    },
     onsubmit() {},
 
     onTambah() {},
